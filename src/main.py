2269cd33c1f8c89a06b90e0df38e06d1cd6cae6c
Rohit Natesh <rohit.natesh95@gmail.com>
2023-08-30 16:19:09 -0400

    Adding some comments

--- src/main.py
+++ src/main.py
@@ -2,33 +2,47 @@ import os
 import pandas as pd
 from mysql.connector import connect, Error
 
+# Get the current directory and compute the absolute path to the 'data' folder.
 current_directory = os.getcwd()
 data_directory_path = os.path.join(current_directory, "data")
 
 # Get file names in 'data' folder.
 data_file_names = os.listdir(data_directory_path)
+# Filtering to get only the files.
+# TODO: Check the file extensions also.
 data_file_names = [
     f for f in data_file_names if os.path.isfile(f"{data_directory_path}/{f}")
 ]
 
+# For each file.
 for file_name in data_file_names:
+    # Read the excel.
     data = pd.read_excel(f"{data_directory_path}/{file_name}")
 
+    # Files are named in the format SCHEMA#TABLE_NAME.
+    # Get the table details from the file name.
     [schema, raw_table] = file_name.split("#")
+    # Remove file extension.
     table = raw_table.split(".")[0]
+    # Get all the column names in the excel, which corresponds to SQL columns names as well.
     columns = data.columns.values.tolist()
 
     try:
+        # Make DB connection.
+        # TODO: The file name can be unique keys to entries in another file having DB details.
         with connect(
             host="localhost",
             user="root",
             password="root",
         ) as connection:
             with connection.cursor() as cursor:
+                # For every row in the excel.
                 for index, row in data.iterrows():
+                    # Try to get the row in the SQL.
                     query = f"SELECT * FROM {schema}.{table} WHERE id={row['id']}"
                     result = cursor.execute(query)
 
+                    # If row exists in SQL, then the existing row should be updated with the new details.
                     if cursor.fetchone():
                         update_query = f"UPDATE {schema}.{table} SET "
 
@@ -39,6 +53,7 @@ for file_name in data_file_names:
 
                         cursor.execute(update_query)
                         connection.commit()
+                    # Else, a new row entry should be created.
                     else:
                         insert_query = f"INSERT INTO {schema}.{table} VALUES ("
 
